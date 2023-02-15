aae01593a1926610f58e8c20265dd639383e780d
mertoatakan <mertoatakan@gmail.com>
2023-02-15 16:37:12 +0300

    StudentController is added to serve as API Layer

--- src/main/java/com/demo/demo/DemoApplication.java
+++ src/main/java/com/demo/demo/DemoApplication.java
@@ -1,38 +1,13 @@
 package com.demo.demo;
 
-import com.demo.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	/*@GetMapping
-	public String hello(){
-		return "Hello World!";
-	}*/
-	@GetMapping
-	public List<Student> hello(){
-		return List.of(
-				new Student(
-						1L,
-						"Mert Atakan Onrat",
-						"mertoatakan@gmail.com",
-						LocalDate.of(1999, Month.SEPTEMBER ,11),
-						24
-				)
-		);
-	}
-
 
 }
