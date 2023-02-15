76a9533aa25d90035a6b1f92df341890a05aea02
mertoatakan <mertoatakan@gmail.com>
2023-02-15 16:28:14 +0300

    Student Class was created and the information of a Student was viewed

--- src/main/java/com/demo/demo/DemoApplication.java
+++ src/main/java/com/demo/demo/DemoApplication.java
@@ -1,10 +1,13 @@
 package com.demo.demo;
 
+import com.demo.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -19,8 +22,16 @@ public class DemoApplication {
 		return "Hello World!";
 	}*/
 	@GetMapping
-	public List<String> hello(){
-		return List.of("Hello", "World");
+	public List<Student> hello(){
+		return List.of(
+				new Student(
+						1L,
+						"Mert Atakan Onrat",
+						"mertoatakan@gmail.com",
+						LocalDate.of(1999, Month.SEPTEMBER ,11),
+						24
+				)
+		);
 	}
 
 
